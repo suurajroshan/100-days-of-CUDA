774f1ba70c0bdc4af7f8aa92e12e1334846041f9
suurajroshan <suuraj.perpeli@fau.de>
2025-02-28 01:01:05 +0100

    matrix-matrix multiplication bugs

--- Day2/matrix-multiplication.cu
+++ Day2/matrix-multiplication.cu
@@ -2,7 +2,7 @@
 // C = A * B
 // Note: A and B are square matrices. This is a naive matrix-matrix multiplication.
 
-// ./<file>.o `r1` `c1/r2` `c2`
+// ./<FILE>.o `r1` `c1/r2` `c2`
 
 #include <stdio.h>
 #include <assert.h>
@@ -13,29 +13,38 @@ void initWith(float num, float *a, int N){
     }
 }
 
-void matMulSerial(float *a, float* b, float* result, int xdim, int ydim){
+void matMulNaiveHost(float *a, float* b, float* result, int r1, int r2, int c2){
     
-    for (int i=0; i < )
+    for (int i=0; i < r1; ++i){
+        for (int j=0; j < r2; ++j){
+            for (int k=0; k < c2; ++k){
+                result[k+i*c2] += a[j+i*r2] * b[k+j*c2];
+            }
+        }
+    }
+
 }
 
 int main(int argc, char *argv[]){
     assert(argc==4);
-    int xdim = atoi(argv[1]), ydim = atoi(argv[2]);
+    int r1 = atoi(argv[1]), r2 = atoi(argv[2]), c2 = atoi(argv[3]);
 
     float *A, *B, *C;
-    int numberOfElements = xdim * ydim;
-    size_t size = sizeof(numberOfElements);
 
-    cudaMallocManaged(&A, size);
-    cudaMallocManaged(&B, size);
-    cudaMallocManaged(&C, size);
+    size_t sizeofA = r1 * r2 * sizeof(float);
+    size_t sizeofB = r2 * c2 * sizeof(float);
+    size_t sizeofC = r1 * c2 * sizeof(float);
 
-    initWith(4, A, numberOfElements);
-    initWith(5, B, numberOfElements);
+    cudaMallocManaged(&A, sizeofA);
+    cudaMallocManaged(&B, sizeofB);
+    cudaMallocManaged(&C, sizeofC);
 
-    for (int i=1; i<numberOfElements+1; ++i){
-        printf("%0.0f ", A[i-1]);
-        if ((i%xdim) == 0){
+    initWith(4, A, r1*r2);
+    initWith(5, B, r2*c2);
+
+    for (int i=1; i<r1*c2+1; ++i){
+        printf("%0.0f ", C[i-1]);
+        if ((i%c2) == 0){
             printf("\n");
             }
     }
